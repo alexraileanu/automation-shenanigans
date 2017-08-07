aa56c098957568b1d9a518cf69d8655bd243193f
Alex <alexraileanu@me.com>
2017-08-07 11:21:36 +0200

    added config for digitalocean firewalls in terraform; removed ufw stuff from ansible

--- terraform/digitalocean.tf
+++ terraform/digitalocean.tf
@@ -52,17 +52,37 @@ resource "digitalocean_firewall" "echo" {
 
     inbound_rule = [
         {
-            protocol    = "tcp"
-            port_range  = "80"
+            protocol         = "tcp"
+            port_range       = "80"
+            source_addresses = ["0.0.0.0/0", "::/0"]
         }, {
-            protocol    = "tcp"
-            port_range  = "443"
+            protocol         = "tcp"
+            port_range       = "443"
+            source_addresses = ["0.0.0.0/0", "::/0"]
         }, {
             protocol         = "tcp"
             port_range       = "222"
             source_addresses = ["213.127.204.188"]
         }, {
-            protocol = "icmp"
+            protocol         = "icmp"
+            source_addresses = ["0.0.0.0/0", "::/0"]
+        }
+    ]
+
+    outbound_rule = [
+        {
+            protocol              = "icmp"
+            destination_addresses = ["0.0.0.0/0", "::/0"]
+        },
+        {
+            protocol              = "tcp"
+            port_range            = "all"
+            destination_addresses = ["0.0.0.0/0", "::/0"]
+        },
+        {
+            protocol              = "udp"
+            port_range            = "all"
+            destination_addresses = ["0.0.0.0/0", "::/0"]
         }
     ]
 
