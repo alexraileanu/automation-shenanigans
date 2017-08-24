263e9b894cfed023136b185e2e417dc5cae2d5c8
Alex <alexraileanu@me.com>
2017-08-24 15:32:13 +0200

    added text for homepage so it doesn't 404; updated gitignore

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app.config.from_pyfile('config/echo.cfg')
 
 redis = FlaskRedis(app)
 
+@app.route('/')
 @app.route('/<echo>')
-def echo(echo):
+def echo(echo = 'hello'):
     redis_stuff = {'id': str(uuid.uuid4()), 'q': echo, 'date': time.time()}
     redis.lpush('echo', redis_stuff)
+    
     return echo
